9388d829d972a9d48f36a048febcf6e9f0c55d14
Petrenko Mikhael <petrenkomi@mail.ru>
2022-01-13 05:08:45 +0000

    update

---
+++ objects/UObjectImplementation.java
@@ -0,0 +1,25 @@
+package Tanks.objects;
+
+import java.util.HashMap;
+
+public class UObjectImplementation implements UObject
+{
+    private HashMap<String, Object> options;
+
+    public UObjectImplementation()
+    {
+        options = new HashMap<>();
+    }
+
+    @Override
+    public void set(String key, Object value)
+    {
+        options.put(key, value);
+    }
+
+    @Override
+    public Object get(String key)
+    {
+        return options.getOrDefault(key, null);
+    }
+}
